2f3504376a1007aeb3ca94497b0e07f4b6a792f2
Dinesh <dineshkumar1deve@gmail.com>
2021-01-06 09:26:50 +0530

    Cucumber Framework skeleton First Commit

---
+++ src/main/java/CucumberFrameworkPrac/App.java
@@ -0,0 +1,13 @@
+package CucumberFrameworkPrac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
